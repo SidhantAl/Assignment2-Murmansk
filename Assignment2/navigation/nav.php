ab9f935a35c1a3bcd000bf5ae920c746f3c538a9
SidhantAl <sidhant.ahluwalia@gmail.com>
2018-02-05 11:26:13 -0500

    Assignment 2 Version 1.0
    Basic AI layout for pages, AI working on readme

---
+++ Assignment2/navigation/nav.php
@@ -0,0 +1,9 @@
+<div class="nav">
+<ul class="navigation">
+  		<li><a href="home.php">Home</a></li>
+  		<li><a href="ai.php">AI</a></li>
+  		<li><a href="designer.php">Designer</a></li>
+  		<li><a href="coder.php">Coder</a></li>
+
+</ul>
+</div>
