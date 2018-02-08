8d07f764b45be2342054391d529c5ad5468aea30
Alexander Punj <35773188+apunj@users.noreply.github.com>
2018-02-08 01:52:59 -0500

    Update nav.php

--- Assignment2/navigation/nav.php
+++ Assignment2/navigation/nav.php
@@ -1,6 +1,6 @@
 <div class="nav">
 <ul class="navigation">
-  		<li><a href="home.php">Home</a></li>
+  		<li><a href="index.php">Home</a></li>
   		<li><a href="ai.php">AI</a></li>
   		<li><a href="designer.php">Designer</a></li>
   		<li><a href="coder.php">Coder</a></li>
